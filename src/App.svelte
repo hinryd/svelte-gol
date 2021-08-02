014c4ad2cd77870e8aadf43a0a1dfb8f8f3cee23
Henry <lkhh@protonmail.com>
2021-08-02 22:30:05 +0800

    Basic functionality

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,90 @@
 <script lang="ts">
-    import Canvas from './components/Canvas.svelte'
-    import Controls from './components/Controls.svelte'
-    import { cellSize, grid } from './game'
+    import PlayIcon from './icons/Play.svelte'
+    import PauseIcon from './icons/Pause.svelte'
+    import ResetIcon from './icons/Reset.svelte'
+    import { onMount } from 'svelte'
+    import { cellSize, next } from './game'
+
+    let playing = false
+    let interval = null
+    let cellRadius = cellSize / 2
+    let currentGrid = Array.from(
+        { length: Math.floor(window.innerHeight / cellSize) },
+        () =>
+            Array.from(
+                { length: Math.floor(window.innerWidth / cellSize) },
+                () => Math.round(Math.random()) as 0 | 1
+            )
+    )
+    let canvas: HTMLCanvasElement
+    let ctx: CanvasRenderingContext2D
+    const offset = (n: number) => n * (cellSize + 2) + cellRadius + 1
+    const draw = (ctx: CanvasRenderingContext2D, grid: (0 | 1)[][]) => {
+        canvas.width = window.innerWidth
+        canvas.height = window.innerHeight
+        grid.forEach((row, y) => {
+            row.forEach((isAlive, x) => {
+                const color = isAlive ? 'black' : 'white'
+                ctx.fillStyle = color
+                ctx.beginPath()
+                ctx.arc(offset(x), offset(y), cellRadius, 0, 2 * Math.PI, false)
+                ctx.fill()
+            })
+        })
+    }
+
+    const onPlay = () => {
+        if (playing) {
+            clearInterval(interval)
+            playing = false
+        } else {
+            interval = setInterval(() => {
+                currentGrid = next(currentGrid)
+                draw(ctx, currentGrid)
+            }, 50)
+            console.log(interval)
+            playing = true
+        }
+    }
+
+    const onRand = () => {
+        if (playing) {
+            clearInterval(interval)
+            playing = false
+        }
+        currentGrid = Array.from(
+            { length: Math.floor(window.innerHeight / cellSize) },
+            () =>
+                Array.from(
+                    { length: Math.floor(window.innerWidth / cellSize) },
+                    () => Math.round(Math.random()) as 0 | 1
+                )
+        )
+        draw(ctx, currentGrid)
+    }
+
+    onMount(() => {
+        ctx = canvas.getContext('2d')
+        onRand()
+    })
 </script>
 
-<main class="relative">
-    <Canvas {cellSize} {grid} />
-    <Controls />
-</main>
+<svelte:window on:resize={onRand} />
+
+<div class="absolute flex justify-center bottom-5 w-full">
+    <div class="flex border shadow-2xl rounded-xl bg-white">
+        <button class="p-7" on:click|preventDefault={onPlay}>
+            {#if playing}
+                <PauseIcon />
+            {:else}
+                <PlayIcon />
+            {/if}
+        </button>
+        <button class="p-7" on:click={onRand}><ResetIcon /></button>
+    </div>
+</div>
+
+<canvas bind:this={canvas} />
 
 <style windi:preflights:global>
 </style>
--- src/game.ts
+++ src/game.ts
@@ -1,15 +1,41 @@
-export const cellSize = 25
+export const cellSize = 7
 
-export const grid = []
-
-const getGrid = (size, width, height) => {
-    const cols = Math.floor(width / size)
-    const rows = Math.floor(height / size)
-    return {
-        grid: Array.from({ length: rows }, () =>
-            Array.from({ length: cols }, () => Math.round(Math.random()))
-        ),
-        rows,
-        cols
+export const next = (grid: (0 | 1)[][]) => {
+    const combinePositions = ({ y, x }): number[][] =>
+        [-1, 0, 1].reduce(
+            (acc, _y, _, offset) =>
+                offset.reduce(
+                    (b, _x) => (_x || _y ? [...b, [y + _y, x + _x]] : b),
+                    acc
+                ),
+            []
+        )
+    const newKey = (size: number) => (key: number) => {
+        if (key === -1) {
+            return size - 1
+        }
+        if (key === size) {
+            return 0
+        }
+        return key
     }
+    const newKeys = (size: number, keys: number[]) => keys.map(newKey(size))
+    const getIn = grid => (position: number[]) =>
+        (([y, x]) => grid[y][x])(newKeys(grid.length, position))
+
+    return grid.map((row, y) =>
+        row.map((isAlive, x) => {
+            const neighbours = combinePositions({ y, x })
+                .map(getIn(grid))
+                .filter(Boolean).length
+
+            if (neighbours == 2) {
+                return isAlive
+            } else if (neighbours == 3) {
+                return 1
+            } else {
+                return 0
+            }
+        })
+    )
 }
