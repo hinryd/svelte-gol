e26032ef6afa69a5d22dfde9849fdaf14fd1c50a
Henry <lkhh@protonmail.com>
2021-08-24 19:22:34 +0800

    Add capacitor to the project

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'dev.hinryd.gol',
+  appName: "Conway's game of life",
+  webDir: 'public',
+  bundledWebRuntime: false
+};
+
+export default config;
